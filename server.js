3f083017516fdafcb04f3c3d9672626b0fd8d5f2
rajatshuklaofficial <rajatshuklaofficial@gmail.com>
2019-01-14 12:37:49 +0530

    connecting mongodb

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+var port = process.env.PORT || 7229
+
+
+const db = require('./config/key').mongoUrl;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Soduko listening on port ${port}!`))
--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 var port = process.env.PORT || 7229
+
+
+const db = require('./config/keys').mongoUrl;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 

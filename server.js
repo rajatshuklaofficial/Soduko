88c7d8fbeac8bf10c12bf66c33d1c004247ff9e5
rajatshuklaofficial <rajatshuklaofficial@gmail.com>
2019-01-14 18:11:02 +0530

    Register and Login

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 var port = process.env.PORT || 7229
 
 
-const db = require('./config/key').mongoUrl;
+
+const users = require('./routes/api/users');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const db = require('./config/keys').mongoUrl;
 
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+
+app.use('/api/users', users);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
